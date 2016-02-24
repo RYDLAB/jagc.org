d329a86dff5916906e9d8cc46ec8c5410a5a4444
Maxim Kolodyazhny <mw26@rydlab.ru>
2016-02-24 17:06:35 +0500

    Updates for language collection

--- etc/mongodb/migrations/003-update-langs.js
+++ etc/mongodb/migrations/003-update-langs.js
@@ -1,3 +1,7 @@
-db.language.remove({name: 'ruby1.8'});
-db.language.remove({name: 'nodejs'});
-db.solution.update({lng: 'ruby1.8'}, {$set: {lng: 'ruby1.9' }}, {multi: true});
+db.language.remove({name: 'ruby1.9'});
+db.language.update({name: 'ruby'}, {$set:{name: 'ruby', path: '/usr/bin/ruby'}}, {upsert : true});
+db.solution.update({lng: 'ruby1.9'}, {$set: {name: 'ruby1.9', path: '/usr/bin/ruby' }}, {multi: true});
+
+db.language.update({name: 'ruby2.0'}, {$set:{name: 'ruby2.0', path: '/usr/bin/ruby2.2'}}, {upsert : true});
+db.language.update({ name: 'python3'}, {$set: {name: 'python3', path: '/usr/bin/python3.5'}}, {upsert:true});
+
